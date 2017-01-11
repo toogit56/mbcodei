f21f3a0c5eeeb7c436aed64c486b0e7a0fb27995
toogit56 <toogit56@gmail.com>
2017-01-11 19:15:23 +0900

    2017/01/11 コミット

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own
 |
 */
-$config['base_url'] = 'http://local.codei-test.jp/';
+$config['base_url'] = 'http://local.mbcodei.jp/';
 
 /*
 |--------------------------------------------------------------------------
